492c92de165695ea2eaa0188101b5d2832a44588
Misbahudin <misbahudin.id@gmail.com>
2023-08-02 19:43:04 +0700

    Where Method

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
@@ -22,7 +23,7 @@ class QueryBuilderTest extends TestCase
         ]);
         DB::table("categories")->insert([
             "id" => "FOOD",
-            "name" => "Food"
+            "name" => "Food" 
         ]);
 
         $resultes = DB::select("select count(id) as total from categories");
@@ -35,6 +36,99 @@ class QueryBuilderTest extends TestCase
         $collection = DB::table("categories")->select(["id", "name"])->get();
         self::assertNotNull($collection);
 
+        $collection->each(function($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function insertCategories() {
+        DB::table("categories")->insert([
+            "id" => "SMARTPHONE",
+            "name" => "Poco M4 Pro",
+            "description" => "Mediatek G96 with Panel AMOLED 90Hz",
+            "created_at" => "2022-03-16"
+        ]);
+        DB::table("categories")->insert([
+            "id" => "FOOD",
+            "name" => "Whiskas",
+            "description" => "Makanan Kucing",
+            "created_at" => "2021-01-11"
+        ]);
+        DB::table("categories")->insert([
+            "id" => "LAPTOP",
+            "name" => "Thinkpad T480",
+            "description" => "intel core i5-8350u with 16gb ram",
+            "created_at" => "2023-07-05"
+        ]);
+        DB::table("categories")->insert([
+            "id" => "FASHION",
+            "name" => "Kemeja",
+            "created_at" => "2020-01-11"
+        ]);
+        DB::table("categories")->insert([
+            "id" => "CAR",
+            "name" => "When Lambo Sir",
+            "created_at" => "2023-05-15"
+        ]);
+    }
+
+    public function testWhere() {
+        $this->insertCategories();
+
+        $collection = DB::table("categories")->where(function(Builder $builder) {
+            $builder->where("id", "=", "SMARTPHONE");
+            $builder->orWhere("id", "=", "LAPTOP");
+            // SELECT * FROM categories WHERE (id = SMARTPHONE OR id = LAPTOP)
+        })->get();
+
+        self::assertCount(2, $collection);
+        
+        $collection->each(function($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function testWhereBetween() {
+        $this->insertCategories();
+
+        $collection = DB::table("categories")->whereBetween("created_at", ["2021-01-10", "2023-08-01"])->get();
+        self::assertCount(4, $collection);
+
+        $collection->each(function($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function testWhereIn() {
+        $this->insertCategories();
+
+        $collection = DB::table("categories")->whereIn("id", ["SMARTPHONE", "LAPTOP"])->get();
+
+        self::assertCount(2, $collection);
+        
+        $collection->each(function($item) {
+            Log::info(json_encode($item));
+        });
+    }
+    public function testWhereNull() {
+        $this->insertCategories();
+
+        $collection = DB::table("categories")->whereNull("description")->get();
+
+        self::assertCount(2, $collection);
+        
+        $collection->each(function($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
+    public function testWhereDate() {
+        $this->insertCategories();
+
+        $collection = DB::table("categories")->whereDate("created_at", "2020-01-11")->get();
+
+        self::assertCount(1, $collection);
+        
         $collection->each(function($item) {
             Log::info(json_encode($item));
         });
