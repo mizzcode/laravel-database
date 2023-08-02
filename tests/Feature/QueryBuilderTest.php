8411267431819805d4deb8104126ce18195b6119
Misbahudin <misbahudin.id@gmail.com>
2023-08-02 18:35:12 +0700

    QueryBuilder Select

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class QueryBuilderTest extends TestCase
@@ -26,5 +27,16 @@ class QueryBuilderTest extends TestCase
 
         $resultes = DB::select("select count(id) as total from categories");
         self::assertEquals(2, $resultes[0]->total);
+    }
+
+    public function testSelect() {
+        $this->testInsert();
+
+        $collection = DB::table("categories")->select(["id", "name"])->get();
+        self::assertNotNull($collection);
+
+        $collection->each(function($item) {
+            Log::info(json_encode($item));
+        });
     }
 }
