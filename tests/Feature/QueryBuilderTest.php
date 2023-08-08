d5ea2b164af0a3f370e1a0218f416c022182adf3
Misbahudin <misbahudin.id@gmail.com>
2023-08-08 20:35:40 +0700

    Query Builder GroupBy

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -330,4 +330,40 @@ class QueryBuilderTest extends TestCase
 
         self::assertEquals(2, $collection[0]->total_product);
     }
+
+    public function insertProductsFood() {
+        DB::table("products")->insert([
+            "id" => "3",
+            "name" => "Whiskas",
+            "price" => 15000,
+            "category_id" => "FOOD"
+        ]);
+        DB::table("products")->insert([
+            "id" => "4",
+            "name" => "Mie Ayam Bakso",
+            "price" => 9000,
+            "category_id" => "FOOD"
+        ]);
+    }
+
+    public function testGroupByHaving() {
+        $this->insertProducts();
+        $this->insertProductsFood();
+
+        $collection = DB::table("products")
+        ->select("category_id", DB::raw("count(*) as total_product"))
+        ->groupBy("category_id")
+        ->orderBy("category_id", "desc")
+        ->get();
+
+        self::assertCount(2, $collection);
+        self::assertEquals("SMARTPHONE", $collection[0]->category_id);
+        self::assertEquals("FOOD", $collection[1]->category_id);
+        self::assertEquals(2, $collection[0]->total_product);
+        self::assertEquals(2, $collection[1]->total_product);
+
+        $collection->each(function($item) {
+            Log::info(json_encode($item));
+        });
+    }
 }
